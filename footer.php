11f1720c7d3f0109609b65f75c1ccd4507381107
Myat Min Lu <myatminlu96@gmail.com>
2019-01-08 13:06:15 +0630

    Ready for Dev

---
+++ footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package VECTOR_MM_Theme
+ */
+
+?>
+	<footer id="colophon" class="site-footer">
+		Copyright © 2019 | All Rights Reserved
+	</footer><!-- #colophon -->
+
+</div><!-- #End of Container  -->
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
